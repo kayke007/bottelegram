590ceeffa8ffe6cceeb9729217184b09f71fcfc9
Kayke Alves da Cruz Silva <201610127@uniprojecao.edu.br>
2016-11-03 21:27:57 -0200

    versao rafael

--- bot_no_telegram.php
+++ bot_no_telegram.php
@@ -1,65 +1,36 @@
-<html>
-    <head>
-        <title>Meu API</title>
-        <meta charset="UTF-8">
-    </head>
-    <body>
+    
         <?php
-       // $file = 'updaeId.txt';
-      //  file_put_contents($file, $updateId . ',', FILE_APPEND | LOCK_X);
+        $file = "message_id.txt";
 
-        //RESPONDE AS MESSAGEM PARA O TELEGRAM.
-        function sendMessage($chatId, $text) {
-            file_get_contents("https://api.telegram.org/bot262367862:AAHEEt28-_QDnzr75NVj-zLvPamcNKXmHHY/sendMessage?chat_id=" . $chatId . "&text=" . $text);
+      //MegaSena
+        for ($i = 1; $i <= 6; $i++) {//gera os 6 numeros
+            $n[] = str_pad(rand(1, 60), 2, '0', STR_PAD_LEFT);
         }
-
-        //FIM DA RESPOSTA.
-        $url = "https://api.telegram.org/bot262367862:AAHEEt28-_QDnzr75NVj-zLvPamcNKXmHHY/getUpdates"; //PEGA PELA URL API DO MEU TELEGRAM
-        $conteudo = file_get_contents($url);
-        $to = json_decode($conteudo, true); //TRATA AS MESSAGEM DO TELEGRAM
-        $run = count($to["result"]);
-        $ids = array();
-        $textos = array();
-        // RECUPERA TEXTO EM IDS
-        for ($i = 0; $i < $run; $i++) {
-            $id = $to["result"][$i]["message"]["chat"]["id"];
-            $texto = $to["result"][$i]["message"]["text"];
-            $data = $to["result"][$i]["message"]["date"];
-            $idTexto = $to["result"][$i]["update_id"];
-
+        sort($n); //ordena os números
+        $mega = implode(' - ', $n); //exibe os numeros
+      //MegaSenafim
+        $url = file_get_contents('https://api.telegram.org/bot262367862:AAHEEt28-_QDnzr75NVj-zLvPamcNKXmHHY/getUpdates');
+        $x = json_decode($url, true);
+        $xLen = count($x['result']);
+        for ($i = 0; $i < $xLen; $i++) {
+            $id = $x['result'][$i]['message']['chat']['id'];
+            $text = $x['result'][$i]['message']['text'];
+            $message = $x['result'][$i]['update_id'];
             $ids[$i] = $id;
-            $textos[$i] = $texto;
-           // echo "Nome: " . $texto . "<br/>";
-           // echo "Nome: " . $data . "<br/>";
+            $texts[$i] = $text;
+            $messages[$i] = $message;
         }
-      //$str = file_get_contents($file);
-      // $arrUpdateId = explode(',', $str);
 
-        $ids = array_unique($ids); //UNIFICA OS ARRAY
-        $ids = array_values($ids); //RETORNA OS VALORES
-        $contagem = count($ids); //AQUI FAZ A CONTAGEM DOS ARRAY UNICOS
-        $contMsg = count($textos);
+        $str = file_get_contents($file);
+        $arrUpdateId = explode(',', $str);
 
-        if (!in_array($updateId, $arrUpdateId)) {
-            echo "Foi enviado sua msg";
-        }
-        //GERA NUMERO MEGASENA
-        for ($c = 0; $c <= 6; $c++) {
-            $numeroMega[] = rand(1, 60);
-        }
-        //SORTIA OS NUMEROS
-        sort($numeroMega);
-        $var1 = implode(" - ", $numeroMega);
-
-        for ($i = 0; $i < $contagem; $i++) {
-
-            for ($p = 0; $p < $contMsg; $p++) {
-                if ($textos[$p] == "/megasena")
-                    sendMessage($ids[$i], "Ola " . $var1);
-                else
-                    sendMessage($ids[$i], "NÃ£o e essa");
+        for ($i = 0; $i < $xLen; $i++) {
+            if (!in_array($messages[$i], $arrUpdateId)) {
+                if ($texts[$i] == '/megaSena') {
+                    $url = 'https://api.telegram.org/bot262367862:AAHEEt28-_QDnzr75NVj-zLvPamcNKXmHHY/sendMessage?chat_id=' . $ids[$i] . "&text=$mega";
+                    file_get_contents($url);
+                    file_put_contents($file, $messages[$i] . ",", FILE_APPEND | LOCK_EX);
+                }
             }
         }
         ?>
-    </body>
-</html>
