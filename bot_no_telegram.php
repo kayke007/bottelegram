a0f1986a4d0a6a8f753de2be8c45cbfcf4203584
201610127 <201610127@EP1435.academico.projecao>
2016-11-17 19:30:42 -0200

    Gitgnore do token e mudança do nome de variáveis!

--- bot_no_telegram.php
+++ bot_no_telegram.php
@@ -1,36 +1,40 @@
-    
-        <?php
-        $file = "message_id.txt";
+<?php
+$file = "message_id.txt";
+$fileToken = "fileToken.txt";
 
-      //MegaSena
-        for ($i = 1; $i <= 6; $i++) {//gera os 6 numeros
-            $n[] = str_pad(rand(1, 60), 2, '0', STR_PAD_LEFT);
-        }
-        sort($n); //ordena os números
-        $mega = implode(' - ', $n); //exibe os numeros
-      //MegaSenafim
-        $url = file_get_contents('https://api.telegram.org/bot262367862:AAHEEt28-_QDnzr75NVj-zLvPamcNKXmHHY/getUpdates');
-        $x = json_decode($url, true);
-        $xLen = count($x['result']);
-        for ($i = 0; $i < $xLen; $i++) {
-            $id = $x['result'][$i]['message']['chat']['id'];
-            $text = $x['result'][$i]['message']['text'];
-            $message = $x['result'][$i]['update_id'];
-            $ids[$i] = $id;
-            $texts[$i] = $text;
-            $messages[$i] = $message;
-        }
+$token = file_get_contents($fileToken);
 
-        $str = file_get_contents($file);
-        $arrUpdateId = explode(',', $str);
+$url = 'https://api.telegram.org/bot' . $token;
 
-        for ($i = 0; $i < $xLen; $i++) {
-            if (!in_array($messages[$i], $arrUpdateId)) {
-                if ($texts[$i] == '/megaSena') {
-                    $url = 'https://api.telegram.org/bot262367862:AAHEEt28-_QDnzr75NVj-zLvPamcNKXmHHY/sendMessage?chat_id=' . $ids[$i] . "&text=$mega";
-                    file_get_contents($url);
-                    file_put_contents($file, $messages[$i] . ",", FILE_APPEND | LOCK_EX);
-                }
-            }
+//MegaSena
+for ($i = 1; $i <= 6; $i++) {//gera os 6 numeros
+    $n[] = str_pad(rand(1, 60), 2, '0', STR_PAD_LEFT);
+}
+sort($n); //ordena os nï¿½meros
+$mega = implode(' - ', $n); //exibe os numeros
+//MegaSenafim
+$request = file_get_contents($url . '/getUpdates');
+$x = json_decode($request, true);
+$xLen = count($x['result']);
+for ($i = 0; $i < $xLen; $i++) {
+    $id = $x['result'][$i]['message']['chat']['id'];
+    $text = $x['result'][$i]['message']['text'];
+    $message = $x['result'][$i]['update_id'];
+    $ids[$i] = $id;
+    $texts[$i] = $text;
+    $messages[$i] = $message;
+}
+
+$str = file_get_contents($file);
+$arrUpdateId = explode(',', $str);
+
+for ($i = 0; $i < $xLen; $i++) {
+    if (!in_array($messages[$i], $arrUpdateId)) {
+        if ($texts[$i] == '/megaSena') {
+            $enviar = $url . '/sendMessage?chat_id=' . $ids[$i] . '&text=' . $mega;
+            file_get_contents($enviar);
+            file_put_contents($file, $messages[$i] . ",", FILE_APPEND | LOCK_EX);
         }
-        ?>
+    }
+}
+?>
